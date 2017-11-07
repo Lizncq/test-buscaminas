68a49003c86d41b18891dd79e24654623cdc16eb
lizncq <lizxd10@gmail.com>
2017-11-07 16:34:29 -0500

    subiendo archivo

---
+++ js/app.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  var board = document.querySelector('.board-js')
+
+  board.addEventListener('click', addX);
+
+  board.removeEventListener('click', addBgc);
+};
+
+var centinel = true;
+
+function addX(event){
+  if (event.target.matches('td') && event.target.textContent === ''){
+    if (centinel)
+      event.target.textContent = 'X';
+    else
+      event.target.textContent = 'O';
+    centinel = !centinel;
+  }
+}
+
+
+
+
+/*board.addEventListener('dblclick', function(event){
+    event.target.style.backgroundColor = 'red';
+});*/
